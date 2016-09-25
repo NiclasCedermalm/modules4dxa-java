ffe902ad484a16ff4b0d7267afdf1c59ed1d8718
Niclas Cedermalm <niclas@cedermalm.se>
2016-09-25 22:15:10 +0200

    Remove old SmartTarget module in favor to the new XO module for SDL Web 8.

--- dxa-module-smarttarget/src/main/java/com/sdl/dxa/modules/smarttarget/analytics/dummy/DummyDataFilter.java
+++
@@ -1,7 +0,0 @@
-package com.sdl.dxa.modules.smarttarget.analytics.dummy;
-
-import com.tridion.smarttarget.analytics.statistics.StatisticsFilter;
-
-public class DummyDataFilter extends StatisticsFilter
-{
-}
